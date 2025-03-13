fae450ebdbfcc194b660b408af949efb7c6676ea
francescomani <email@francescomani.it>
2025-03-13 19:12:36 +0100

    use DCIX0 in commmon search space in any scenario before reconfiguration complete
    this allows to remove a lot of complicated procedures relying on flags and timers
    to verify the RLC re-establishment

--- openair2/RRC/NR/rrc_gNB_mobility.c
+++ openair2/RRC/NR/rrc_gNB_mobility.c
@@ -228,7 +228,6 @@ static void rrc_gNB_trigger_reconfiguration_for_handover(gNB_RRC_INST *rrc, gNB_
       .plmn.mnc_digit_length = rrc->configuration.mnc_digit_length[0],
       .nr_cellid = rrc->nr_cellid, // TODO target cell ID
       .servCellId = 0, // TODO: correct value?
-      .ReconfigComplOutcome = RRCreconf_success,
       .transm_action_ind = &transmission_action_indicator,
   };
   deliver_ue_ctxt_modification_data_t data = {.rrc = rrc,
